56e9b94bf2c19623b629ee6e08b9ac4bc4d3dc69
Orchid23 <calliet0715@gmail.com>
2020-02-16 12:48:31 -0600

    Setup of questions and requires

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+let generateReadme = require("./utils/generateMarkdown");
+let api = require("./utils/api"); 
+
+let userQuestions = (questions) => {
+
+  return inquirer.prompt([{
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What it your project's name?"
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project?"
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?"
+    },
+    {
+      type: "list",
+      name: "license",
+      choices: ["GPL", "MIT", "BSD"]
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "What command should be run to install dependencies?",
+      default: "npm i" //will be inserted if user does not input an answer
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+      default: "npm test" // will be inserted if user does not input an answer
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "What does the user need to know about contributing to the repo?"
+    },
+  
