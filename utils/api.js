554493a45ad82db82dc9d6cf6c267bdd80948302
Orchid23 <calliet0715@gmail.com>
2020-02-16 12:51:20 -0600

    Setup api call

--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,9 @@
-const api = {
-    getUser(username) {
-  
-    }
-  };
-  
-  module.exports = api;
-  
+const axios = require("axios");
+
+//axios api to get the url with users' github username inserted
+function api(github) {
+    return axios
+        .get(`https://api.github.com/users/${github}`)
+}
+
+module.exports = api
