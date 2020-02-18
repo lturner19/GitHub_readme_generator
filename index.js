902b37357e6318eb63197182ce07e29c17bd76c4
Orchid23 <calliet0715@gmail.com>
2020-02-18 17:14:30 -0600

    Renaming of generated readme file

--- index.js
+++ index.js
@@ -49,18 +49,21 @@ let userQuestions = (questions) => {
             name: "contribute",
             message: "What does the user need to know about contributing to the repo?"
         },
+        //allows the user to input responses, then the api is called, data obtained, and then info is generated into the readme file
     ]).then(function (input) {
-
+        
+        //grabs github username, then grabs the info from the github api
         api(input.github).then(function (res) {
             let avatar_url = res.data.avatar_url
             let html_url = res.data.html_url
             let readMe = generateReadme(input, avatar_url, html_url);
 
-            fs.writeFile("README.md", readMe, function (err) {
+    
+            fs.writeFile("README-generated.md", readMe, function (err) {//last argument is a call back
                 if (err) {
                     console.log(err);
                 }
-                console.log("Successfully written to README.md")
+                console.log("Successfully written to README-generated.md")
             })
         })
     })
